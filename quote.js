2609abf527927f8984082bd56850362c9ce0545b
annehong <annehongitp@gmail.com>
2014-11-25 21:15:11 -0800

    added button and bootstrap

--- quote.js
+++ quote.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
 	//alert("hello world");
 
+	var fetch = function() 
+	{
+		alert(1)
+		$.get("https://api.github.com/zen", fetchApi)
+	}
+
+	$("#quote").click(fetch)
+
+	
+
 	var fetchApi = function(data){
 		//alert(data)
 		$(".quote").text(data)
 	}
 
-	$.get("https://api.github.com/zen", fetchApi)
+	//$.get("https://api.github.com/zen", fetchApi)
 	//$.get("https://tranquil-headland-8146.herokuapp.com/zen.json", fetchApi)
 });
 
