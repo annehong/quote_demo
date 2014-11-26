c9bc98aa858bb09226f73fd649c9995a4d58d0fc
annehong <annehongitp@gmail.com>
2014-11-25 20:33:02 -0800

    fetch quote

--- quote.js
+++ quote.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
-	alert("hello world");
-})
+	//alert("hello world");
+
+	var fetchApi = function(data){
+		//alert(data)
+		$(".quote").text(data)
+	}
+
+	$.get("https://api.github.com/zen", fetchApi)
+	//$.get("https://tranquil-headland-8146.herokuapp.com/zen.json", fetchApi)
+});
 
